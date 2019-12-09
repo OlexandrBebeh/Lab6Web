1179960b181b4828ac3161e2e3f49391ae036838
Olexandrbebekh <olexandrbebeh@gmail.com>
2019-12-09 21:54:28 +0200

    Try to fix heroku and user

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://OlexandrBebeh1:v@cluster0-ous9y.mongodb.net/TryArticles?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://OlexandrBebeh1:QWvKfForRYxCXzUb@cluster0-ous9y.mongodb.net/TryArticles?retryWrites=true&w=majority';
 //const URI = 'localhost3000:mongodb://localhost:27017/'
 const connectDB = async () => {
     await mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true }, () => console.log('db conected'));
